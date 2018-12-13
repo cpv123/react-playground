d71579eb75dfcd7a38798d02cd81b1e83d9adda2
cpv123 <c.vibert@hotmail.com>
2018-12-13 19:57:12 +0000

    Testing mui-virtualized-table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Main from './Main';
+import Table from './Table';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Switch>
             <Route exact path="/" component={Main} />
+            <Route path="/table" component={Table} />
         </Switch>
       </div>
     );
