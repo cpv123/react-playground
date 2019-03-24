b6c004904917339368e88fc0e425d4fbf183ee22
cpv123 <c.vibert@hotmail.com>
2019-03-24 11:09:41 +0000

    Setting up basics form Simple React forms, introducing validation.

---
+++ src/forms/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BasicFormOne from './BasicFormOne';
+import BasicFormTwo from './BasicFormTwo';
+import BasicFormThree from './BasicFormThree';
+
+function Forms() {
+    return (
+        <div>
+            <BasicFormOne />
+            <BasicFormTwo />
+            <BasicFormThree />
+        </div>
+    )
+}
+
+export default Forms;
