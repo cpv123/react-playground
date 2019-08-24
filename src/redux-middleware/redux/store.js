6aeb56f34d948f372b9cd2b4aadaa1ceb704edcc
Chris Vibert <chris.vibert@gousto.co.uk>
2019-08-24 21:17:02 +0100

    update devtools syntax

--- src/redux-middleware/redux/store.js
+++ src/redux-middleware/redux/store.js
@@ -1,10 +1,10 @@
 import thunk from 'redux-thunk'
-import myMiddleware from './custom-middleware'
+import { myMiddleware } from './custom-middleware'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
 
 const createStoreWithMiddleware = compose(applyMiddleware(thunk, myMiddleware),
-	window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
+	window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)(createStore);
   
 const store = createStoreWithMiddleware(reducer);
 
