340153d783d81b8111b249096a138dd793c58024
Chris Vibert <chris.vibert@gousto.co.uk>
2019-09-04 08:12:45 +0100

    Check if phone number is valid

--- src/libphonenumber/index.js
+++ src/libphonenumber/index.js
@@ -4,16 +4,31 @@ import examples from 'libphonenumber-js/examples.mobile.json'
 
 function ExampleNumber() {
   const [exampleNumber, setExampleNumber] = useState(null)
+  const [testNumber, setTestNumber] = useState('')
+  const [testNumberIsValid, setTestNumberIsValid] = useState(false)
+
   const generateExampleNumber = () => {
     const exampleNumber = getExampleNumber('GB', examples).number
     setExampleNumber(exampleNumber)
-    console.log(isValidNumber(exampleNumber))
+  }
+
+  const testNumberFn = () => {
+    const isValid = isValidNumber(testNumber)
+    setTestNumberIsValid(isValid)
   }
 
   return (
     <div>
       <p>Example number is: {exampleNumber}</p>
       <button onClick={generateExampleNumber}>Generate number</button>
+      <div style={{ marginTop: 20 }}>
+        <input
+          onChange={e => setTestNumber(e.target.value)}
+          placehlder='Enter number to check'
+        />
+        <button onClick={testNumberFn}>Check if valid</button>
+        <p>Is valid: {JSON.stringify(testNumberIsValid)}</p>
+      </div>
     </div>
   )
 }
