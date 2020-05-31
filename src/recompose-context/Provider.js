09834887a940e659b66816e818b3367df66b736d
cpv123 <cp.vibert@gmail.com>
2020-05-31 09:06:29 +0700

    Renaming HOCs

--- src/recompose-context/Provider.js
+++ src/recompose-context/Provider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose, withContext, withProps } from 'recompose';
+import { compose, withContext, withProps, setDisplayName } from 'recompose';
 import { ChildOne } from './Children';
 
 // Parent component which provides the context
@@ -8,16 +8,25 @@ const Provider = ({ store }) => (
     <ChildOne />
 );
 
-export default compose(
-    // An example 'global store'.
-    withProps({ 
-        store: {
-            data: [1, 2, 3],
-            isAdmin: true,
-        }
-    }),
-    withContext(
-        { store: PropTypes.object },
-        (props) => ({ store: props.store })
-    )
-)(Provider);
+const withCusomHoc = () => compose(
+  setDisplayName('WithCustomHoc'),
+  withProps(() => {
+    return { first: 'value' }
+  })
+)
+
+const enhance = compose(
+  withCusomHoc(),
+  withProps({
+    store: {
+      data: [1, 2, 3],
+      isAdmin: true
+    }
+  }),
+  withContext(
+    { store: PropTypes.object },
+    (props) => ({ store: props.store })
+  )
+)
+
+export default enhance(Provider);
